8d78ffa12b8cab05de11b58593e7424946a6cef1
Flora2020 <rossignols1989@gmail.com>
2021-03-18 19:48:56 +0800

    feat: users can save their todo at edit page

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -18,6 +18,26 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  const { name, isDone } = req.body
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      if (!todo) {
+        console.log('todo not found.')
+        return res.redirect('/')
+      }
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      todo.updatedAt = new Date()
+      return todo.save()
+        .then(() => res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
+    })
+})
+
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   const UserId = req.user.id
