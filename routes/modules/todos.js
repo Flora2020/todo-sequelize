079239c2b9b4a7cc7e543bcb34d7fa52a311a9b6
Flora2020 <rossignols1989@gmail.com>
2021-03-18 11:45:54 +0800

    feat: users can only access todos belong to them

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -6,8 +6,15 @@ const Todo = db.Todo
 
 router.get('/:id', (req, res) => {
   const id = req.params.id
-  return Todo.findByPk(id)
-    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+  const UserId = req.user.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      if (!todo) {
+        console.log('todo not found.')
+        return res.redirect('/')
+      }
+      res.render('detail', { todo: todo.toJSON() })
+    })
     .catch(error => console.log(error))
 })
 
