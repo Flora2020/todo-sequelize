6139cd24243868f77d9fc59666509de7cac48b2a
Flora2020 <rossignols1989@gmail.com>
2021-03-18 23:36:08 +0800

    feat: users can delete a todo

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -50,6 +50,17 @@ router.put('/:id', (req, res) => {
     })
 })
 
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  const UserId = req.user.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then((todo) => {
+      todo.destroy()
+      return res.redirect('/')
+    })
+    .catch(error => console.log(error))
+})
+
 router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   const UserId = req.user.id
